18b06b88a1e3ce50859d1bf1d4b036eadbc620f5
Robert Snedegar <viking@gmail.com>
2014-04-09 12:07:00 +1000

    s2: Add Cap type.
    Signed-off-by: David Symonds <dsymonds@golang.org>

---
+++ s2/cap.go
@@ -0,0 +1,219 @@
+package s2
+
+import (
+	"fmt"
+	"math"
+
+	"code.google.com/p/gos2/s1"
+)
+
+const (
+	emptyHeight = -1.0
+	zeroHeight  = 0.0
+	fullHeight  = 2.0
+)
+
+var (
+	// centerPoint is the default center for S2Caps
+	centerPoint = Point{PointFromCoords(1.0, 0, 0).Normalize()}
+)
+
+// Cap represents a disc-shaped region defined by a center and radius.
+// Technically this shape is called a "spherical cap" (rather than disc)
+// because it is not planar; the cap represents a portion of the sphere that
+// has been cut off by a plane. The boundary of the cap is the circle defined
+// by the intersection of the sphere and the plane. For containment purposes,
+// the cap is a closed set, i.e. it contains its boundary.
+//
+// For the most part, you can use a spherical cap wherever you would use a
+// disc in planar geometry. The radius of the cap is measured along the
+// surface of the sphere (rather than the straight-line distance through the
+// interior). Thus a cap of radius π/2 is a hemisphere, and a cap of radius
+// π covers the entire sphere.
+//
+// The center is a point on the surface of the unit sphere. (Hence the need for
+// it to be of unit length.)
+//
+// Internally, the cap is represented by its center and "height". The height
+// is the distance from the center point to the cutoff plane. This
+// representation is much more efficient for containment tests than the
+// (center, radius) representation. There is also support for "empty" and
+// "full" caps, which contain no points and all points respectively.
+//
+// The zero value of Cap is an invalid cap. Use EmptyCap to get a valid empty cap.
+type Cap struct {
+	center Point
+	height float64
+}
+
+// CapFromPoint constructs a cap containing a single point.
+func CapFromPoint(p Point) Cap {
+	return CapFromCenterHeight(p, zeroHeight)
+}
+
+// CapFromCenterAngle constructs a cap with the given center and angle.
+func CapFromCenterAngle(center Point, angle s1.Angle) Cap {
+	return CapFromCenterHeight(center, radiusToHeight(angle))
+}
+
+// CapFromCenterHeight constructs a cap with the given center and height. A
+// negative height yields an empty cap; a height of 2 or more yields a full cap.
+func CapFromCenterHeight(center Point, height float64) Cap {
+	return Cap{
+		center: Point{center.Normalize()},
+		height: height,
+	}
+}
+
+// CapFromCenterArea constructs a cap with the given center and surface area.
+// Note that the area can also be interpreted as the solid angle subtended by the
+// cap (because the sphere has unit radius). A negative area yields an empty cap;
+// an area of 4*π or more yields a full cap.
+func CapFromCenterArea(center Point, area float64) Cap {
+	return CapFromCenterHeight(center, area/(2.0*math.Pi))
+}
+
+// EmptyCap returns a cap that contains no points.
+func EmptyCap() Cap {
+	return CapFromCenterHeight(centerPoint, emptyHeight)
+}
+
+// FullCap returns a cap that contains all points.
+func FullCap() Cap {
+	return CapFromCenterHeight(centerPoint, fullHeight)
+}
+
+// IsValid reports whether the Cap is considered valid.
+// Heights are normalized so that they do not exceed 2.
+func (c Cap) IsValid() bool {
+	return c.center.Vector.IsUnit() && c.height <= fullHeight
+}
+
+// IsEmpty reports whether the cap is empty, i.e. it contains no points.
+func (c Cap) IsEmpty() bool {
+	return c.height < zeroHeight
+}
+
+// IsFull reports whether the cap is full, i.e. it contains all points.
+func (c Cap) IsFull() bool {
+	return c.height == fullHeight
+}
+
+// Radius returns the cap's radius.
+func (c Cap) Radius() s1.Angle {
+	if c.IsEmpty() {
+		return s1.Angle(emptyHeight)
+	}
+
+	// This could also be computed as acos(1 - height_), but the following
+	// formula is much more accurate when the cap height is small. It
+	// follows from the relationship h = 1 - cos(r) = 2 sin^2(r/2).
+	return s1.Angle(2 * math.Asin(math.Sqrt(0.5*c.height)))
+}
+
+// Area returns the surface area of the Cap on the unit sphere.
+func (c Cap) Area() float64 {
+	return 2.0 * math.Pi * math.Max(zeroHeight, c.height)
+}
+
+// Contains reports whether this cap contains the other.
+func (c Cap) Contains(other Cap) bool {
+	// In a set containment sense, every cap contains the empty cap.
+	if c.IsFull() || other.IsEmpty() {
+		return true
+	}
+	return c.Radius() >= c.center.Distance(other.center)+other.Radius()
+}
+
+// Intersects reports whether this cap intersects the other cap.
+// i.e. whether they have any points in common.
+func (c Cap) Intersects(other Cap) bool {
+	if c.IsEmpty() || other.IsEmpty() {
+		return false
+	}
+
+	return c.Radius()+other.Radius() >= c.center.Distance(other.center)
+}
+
+// InteriorIntersects reports whether this caps interior intersects the other cap.
+func (c Cap) InteriorIntersects(other Cap) bool {
+	// Make sure this cap has an interior and the other cap is non-empty.
+	if c.height <= zeroHeight || other.IsEmpty() {
+		return false
+	}
+
+	return c.Radius()+other.Radius() > c.center.Distance(other.center)
+}
+
+// ContainsPoint reports whether this cap contains the point.
+func (c Cap) ContainsPoint(p Point) bool {
+	return c.center.Sub(p.Vector).Norm2() <= 2*c.height
+}
+
+// InteriorContainsPoint reports whether the point is within the interior of this cap.
+func (c Cap) InteriorContainsPoint(p Point) bool {
+	return c.IsFull() || c.center.Sub(p.Vector).Norm2() < 2*c.height
+}
+
+// Complement returns the complement of the interior of the cap. A cap and its
+// complement have the same boundary but do not share any interior points.
+// The complement operator is not a bijection because the complement of a
+// singleton cap (containing a single point) is the same as the complement
+// of an empty cap.
+func (c Cap) Complement() Cap {
+	height := emptyHeight
+	if !c.IsFull() {
+		height = fullHeight - math.Max(c.height, zeroHeight)
+	}
+	return CapFromCenterHeight(Point{c.center.Mul(-1.0)}, height)
+}
+
+// CapBound returns a bounding spherical cap. This is not guaranteed to be exact.
+func (c Cap) CapBound() Cap {
+	return c
+}
+
+// ApproxEqual reports if this caps' center and height are within
+// a reasonable epsilon from the other cap.
+func (c Cap) ApproxEqual(other Cap) bool {
+	const epsilon = 1e-14
+	return c.center.ApproxEqual(other.center) &&
+		math.Abs(c.height-other.height) <= epsilon ||
+		c.IsEmpty() && other.height <= epsilon ||
+		other.IsEmpty() && c.height <= epsilon ||
+		c.IsFull() && other.height >= 2-epsilon ||
+		other.IsFull() && c.height >= 2-epsilon
+}
+
+// Expanded returns a new cap expanded by the given angle. If the cap is empty,
+// it returns an empty cap.
+func (c Cap) Expanded(distance s1.Angle) Cap {
+	if c.IsEmpty() {
+		return EmptyCap()
+	}
+	return CapFromCenterAngle(c.center, c.Radius()+distance)
+}
+
+func (c Cap) String() string {
+	return fmt.Sprintf("[Center=%v, Radius=%f]", c.center.Vector, c.Radius().Degrees())
+}
+
+// radiusToHeight converts an s1.Angle into the height of the cap.
+func radiusToHeight(r s1.Angle) float64 {
+	if r.Radians() < 0 {
+		return emptyHeight
+	}
+	if r.Radians() >= math.Pi {
+		return fullHeight
+	}
+	// The height of the cap can be computed as 1 - cos(r), but this isn't very
+	// accurate for angles close to zero (where cos(r) is almost 1). The
+	// formula below has much better precision.
+	d := math.Sin(0.5 * r.Radians())
+	return 2 * d * d
+
+}
+
+// TODO(roberts): Differences from C++
+// AddPoint, AddCap, RectBounds, S2Cell variations of the above
+// -- others
---
+++ s2/cap_test.go
@@ -0,0 +1,256 @@
+package s2
+
+import (
+	"math"
+	"testing"
+
+	"code.google.com/p/gos2/s1"
+)
+
+const (
+	epsilon = 1e-14
+	tinyRad = 1e-10
+)
+
+var (
+	empty      = EmptyCap()
+	full       = FullCap()
+	defaultCap = EmptyCap()
+
+	xAxisPt = PointFromCoords(1, 0, 0)
+	yAxisPt = PointFromCoords(0, 1, 0)
+
+	xAxis = CapFromPoint(xAxisPt)
+	yAxis = CapFromPoint(yAxisPt)
+	xComp = xAxis.Complement()
+
+	hemi    = CapFromCenterHeight(Point{PointFromCoords(1, 0, 1).Normalize()}, 1)
+	concave = CapFromCenterAngle(PointFromLatLng(LatLngFromDegrees(80, 10)),
+		s1.Angle(150.0)*s1.Degree)
+	tiny = CapFromCenterAngle(Point{PointFromCoords(1, 2, 3).Normalize()},
+		s1.Angle(tinyRad))
+)
+
+func TestCapBasicEmptyFullValid(t *testing.T) {
+	tests := []struct {
+		got                Cap
+		empty, full, valid bool
+	}{
+		{Cap{}, false, false, false},
+
+		{empty, true, false, true},
+		{empty.Complement(), false, true, true},
+		{full, false, true, true},
+		{full.Complement(), true, false, true},
+		{defaultCap, true, false, true},
+
+		{xComp, false, true, true},
+		{xComp.Complement(), true, false, true},
+
+		{tiny, false, false, true},
+		{concave, false, false, true},
+		{hemi, false, false, true},
+		{tiny, false, false, true},
+	}
+	for _, test := range tests {
+		if e := test.got.IsEmpty(); e != test.empty {
+			t.Errorf("%v.IsEmpty() = %t; want %t", test.got, e, test.empty)
+		}
+		if f := test.got.IsFull(); f != test.full {
+			t.Errorf("%v.IsFull() = %t; want %t", test.got, f, test.full)
+		}
+		if v := test.got.IsValid(); v != test.valid {
+			t.Errorf("%v.IsValid() = %t; want %t", test.got, v, test.valid)
+		}
+	}
+}
+
+func TestCapCenterHeightRadius(t *testing.T) {
+	if !xAxis.ApproxEqual(xAxis.Complement().Complement()) {
+		t.Errorf("the double complement should equal itself, %v == %v",
+			xAxis, xAxis.Complement().Complement())
+	}
+
+	if full.height != fullHeight {
+		t.Error("full Caps should be full height")
+	}
+	if full.Radius().Degrees() != 180.0 {
+		t.Error("radius of x-axis cap should be 180 degrees")
+	}
+
+	if empty.center != defaultCap.center {
+		t.Error("empty Caps should be have the same center as the default")
+	}
+	if empty.height != defaultCap.height {
+		t.Error("empty Caps should be have the same height as the default")
+	}
+
+	if yAxis.height != zeroHeight {
+		t.Error("y-axis cap should not be empty height")
+	}
+
+	if xAxis.height != zeroHeight {
+		t.Error("x-axis cap should not be empty height")
+	}
+	if xAxis.Radius().Radians() != zeroHeight {
+		t.Errorf("radius of x-axis cap got %f want %f", xAxis.Radius().Radians(), emptyHeight)
+	}
+
+	hc := Point{hemi.center.Mul(-1.0)}
+	if hc != hemi.Complement().center {
+		t.Error("hemi center and its complement should have the same center")
+	}
+	if hemi.height != 1.0 {
+		t.Error("hemi cap should be 1.0 in height")
+	}
+}
+
+func TestCapContains(t *testing.T) {
+	tests := []struct {
+		c1, c2 Cap
+		want   bool
+	}{
+		{empty, empty, true},
+		{full, empty, true},
+		{full, full, true},
+		{empty, xAxis, false},
+		{full, xAxis, true},
+		{xAxis, full, false},
+		{xAxis, xAxis, true},
+		{xAxis, empty, true},
+		{hemi, tiny, true},
+		{hemi, CapFromCenterAngle(xAxisPt, s1.Angle(math.Pi/4-epsilon)), true},
+		{hemi, CapFromCenterAngle(xAxisPt, s1.Angle(math.Pi/4+epsilon)), false},
+		{concave, hemi, true},
+		{concave, CapFromCenterHeight(Point{concave.center.Mul(-1.0)}, 0.1), false},
+	}
+	for _, test := range tests {
+		if got := test.c1.Contains(test.c2); got != test.want {
+			t.Errorf("%v.Contains(%v) = %t; want %t", test.c1, test.c2, got, test.want)
+		}
+	}
+}
+
+func TestCapContainsPoint(t *testing.T) {
+	tangent := tiny.center.Cross(PointFromCoords(3, 2, 1).Vector).Normalize()
+	tests := []struct {
+		c    Cap
+		p    Point
+		want bool
+	}{
+		{xAxis, xAxisPt, true},
+		{xAxis, PointFromCoords(1, 1e-20, 0), false},
+		{yAxis, xAxis.center, false},
+		{xComp, xAxis.center, true},
+		{xComp.Complement(), xAxis.center, false},
+		{tiny, Point{tiny.center.Add(tangent.Mul(tinyRad * 0.99))}, true},
+		{tiny, Point{tiny.center.Add(tangent.Mul(tinyRad * 1.01))}, false},
+		{hemi, Point{PointFromCoords(1, 0, -(1 - epsilon)).Normalize()}, true},
+		{hemi, xAxisPt, true},
+		{hemi.Complement(), xAxisPt, false},
+		{concave, PointFromLatLng(LatLngFromDegrees(-70*(1-epsilon), 10)), true},
+		{concave, PointFromLatLng(LatLngFromDegrees(-70*(1+epsilon), 10)), false},
+		{concave, PointFromLatLng(LatLngFromDegrees(-50*(1-epsilon), -170)), true},
+		{concave, PointFromLatLng(LatLngFromDegrees(-50*(1+epsilon), -170)), false},
+	}
+	for _, test := range tests {
+		if got := test.c.ContainsPoint(test.p); got != test.want {
+			t.Errorf("%v.ContainsPoint(%v) = %t, want %t", test.c, test.p, got, test.want)
+		}
+	}
+}
+
+func TestCapInteriorIntersects(t *testing.T) {
+	tests := []struct {
+		c1, c2 Cap
+		want   bool
+	}{
+		{empty, empty, false},
+		{empty, xAxis, false},
+		{full, empty, false},
+		{full, full, true},
+		{full, xAxis, true},
+		{xAxis, full, false},
+		{xAxis, xAxis, false},
+		{xAxis, empty, false},
+		{concave, hemi.Complement(), true},
+	}
+	for _, test := range tests {
+		if got := test.c1.InteriorIntersects(test.c2); got != test.want {
+			t.Errorf("%v.InteriorIntersects(%v); got %t want %t", test.c1, test.c2, got, test.want)
+		}
+	}
+}
+
+func TestCapInteriorContains(t *testing.T) {
+	if hemi.InteriorContainsPoint(Point{PointFromCoords(1, 0, -(1 + epsilon)).Normalize()}) {
+		t.Errorf("hemi (%v) should not contain point just past half way(%v)", hemi,
+			Point{PointFromCoords(1, 0, -(1 + epsilon)).Normalize()})
+	}
+}
+
+func TestCapExpanded(t *testing.T) {
+	cap50 := CapFromCenterAngle(xAxisPt, 50.0*s1.Degree)
+	cap51 := CapFromCenterAngle(xAxisPt, 51.0*s1.Degree)
+
+	if !empty.Expanded(s1.Angle(fullHeight)).IsEmpty() {
+		t.Error("Expanding empty cap should return an empty cap")
+	}
+	if !full.Expanded(s1.Angle(fullHeight)).IsFull() {
+		t.Error("Expanding a full cap should return an full cap")
+	}
+
+	if !cap50.Expanded(0).ApproxEqual(cap50) {
+		t.Error("Expanding a cap by 0° should be equal to the original")
+	}
+	if !cap50.Expanded(1 * s1.Degree).ApproxEqual(cap51) {
+		t.Error("Expanding 50° by 1° should equal the 51° cap")
+	}
+
+	if cap50.Expanded(129.99 * s1.Degree).IsFull() {
+		t.Error("Expanding 50° by 129.99° should not give a full cap")
+	}
+	if !cap50.Expanded(130.01 * s1.Degree).IsFull() {
+		t.Error("Expanding 50° by 130.01° should give a full cap")
+	}
+}
+
+func TestRadiusToHeight(t *testing.T) {
+	tests := []struct {
+		got  s1.Angle
+		want float64
+	}{
+		// Above/below boundary checks.
+		{s1.Angle(-0.5), emptyHeight},
+		{s1.Angle(0), 0},
+		{s1.Angle(math.Pi), fullHeight},
+		{s1.Angle(2 * math.Pi), fullHeight},
+		// Degree tests.
+		{-7.0 * s1.Degree, emptyHeight},
+		{-0.0 * s1.Degree, 0},
+		{0.0 * s1.Degree, 0},
+		{12.0 * s1.Degree, 0.02185239926619},
+		{30.0 * s1.Degree, 0.13397459621556},
+		{45.0 * s1.Degree, 0.29289321881345},
+		{90.0 * s1.Degree, 1.0},
+		{179.99 * s1.Degree, 1.99999998476912},
+		{180.0 * s1.Degree, fullHeight},
+		{270.0 * s1.Degree, fullHeight},
+		// Radians tests.
+		{-1.0 * s1.Radian, emptyHeight},
+		{-0.0 * s1.Radian, 0},
+		{0.0 * s1.Radian, 0},
+		{1.0 * s1.Radian, 0.45969769413186},
+		{math.Pi / 2.0 * s1.Radian, 1.0},
+		{2.0 * s1.Radian, 1.41614683654714},
+		{3.0 * s1.Radian, 1.98999249660044},
+		{math.Pi * s1.Radian, fullHeight},
+		{4.0 * s1.Radian, fullHeight},
+	}
+	for _, test := range tests {
+		// float64Eq comes from s2latlng_test.go
+		if got := radiusToHeight(test.got); !float64Eq(got, test.want) {
+			t.Errorf("radiusToHeight(%v) = %v; want %v", test.got, got, test.want)
+		}
+	}
+}
