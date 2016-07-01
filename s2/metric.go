ae8e0670b8a8d36e4f65d0816710696a61287454
Robert Snedegar <viking@gmail.com>
2016-07-01 11:37:00 +1000

    s2: Add more metrics and related documentation.
    Signed-off-by: David Symonds <dsymonds@golang.org>

--- s2/metric.go
+++ s2/metric.go
@@ -22,7 +22,13 @@ import (
 	"math"
 )
 
-// A Metric is a measure for cells.
+// A Metric is a measure for cells. It is used to describe the shape and size
+// of cells. They are useful for deciding which cell level to use in order to
+// satisfy a given condition (e.g. that cell vertices must be no further than
+// "x" apart). You can use the Value(level) method to compute the corresponding
+// length or area on the unit sphere for cells at a given level. The minimum
+// and maximum bounds are valid for cells at all levels, but they may be
+// somewhat conservative for very large cells (e.g. face cells).
 type Metric struct {
 	// Dim is either 1 or 2, for a 1D or 2D metric respectively.
 	Dim int
@@ -31,18 +37,74 @@ type Metric struct {
 }
 
 // Defined metrics.
-// We only support the quadratic projection.
+// Of the projection methods defined in C++, Go only supports the quadratic projection.
+
+// Each cell is bounded by four planes passing through its four edges and
+// the center of the sphere. These metrics relate to the angle between each
+// pair of opposite bounding planes, or equivalently, between the planes
+// corresponding to two different s-values or two different t-values.
+var (
+	MinAngleSpanMetric = Metric{1, 4.0 / 3}
+	AvgAngleSpanMetric = Metric{1, math.Pi / 2}
+	MaxAngleSpanMetric = Metric{1, 1.704897179199218452}
+)
+
+// The width of geometric figure is defined as the distance between two
+// parallel bounding lines in a given direction. For cells, the minimum
+// width is always attained between two opposite edges, and the maximum
+// width is attained between two opposite vertices. However, for our
+// purposes we redefine the width of a cell as the perpendicular distance
+// between a pair of opposite edges. A cell therefore has two widths, one
+// in each direction. The minimum width according to this definition agrees
+// with the classic geometric one, but the maximum width is different. (The
+// maximum geometric width corresponds to MaxDiag defined below.)
+//
+// The average width in both directions for all cells at level k is approximately
+// AvgWidthMetric.Value(k).
+//
+// The width is useful for bounding the minimum or maximum distance from a
+// point on one edge of a cell to the closest point on the opposite edge.
+// For example, this is useful when growing regions by a fixed distance.
 var (
 	MinWidthMetric = Metric{1, 2 * math.Sqrt2 / 3}
-	MaxWidthMetric = Metric{1, 1.704897179199218452}
+	AvgWidthMetric = Metric{1, 1.434523672886099389}
+	MaxWidthMetric = Metric{1, MaxAngleSpanMetric.Deriv}
+)
+
+// The edge length metrics can be used to bound the minimum, maximum,
+// or average distance from the center of one cell to the center of one of
+// its edge neighbors. In particular, it can be used to bound the distance
+// between adjacent cell centers along the space-filling Hilbert curve for
+// cells at any given level.
+var (
+	MinEdgeMetric = Metric{1, 2 * math.Sqrt2 / 3}
+	AvgEdgeMetric = Metric{1, 1.459213746386106062}
+	MaxEdgeMetric = Metric{1, MaxAngleSpanMetric.Deriv}
+
+	// MaxEdgeAspect is the maximum edge aspect ratio over all cells at any level,
+	// where the edge aspect ratio of a cell is defined as the ratio of its longest
+	// edge length to its shortest edge length.
+	MaxEdgeAspect = 1.442615274452682920
 
 	MinAreaMetric = Metric{2, 8 * math.Sqrt2 / 9}
 	AvgAreaMetric = Metric{2, 4 * math.Pi / 6}
 	MaxAreaMetric = Metric{2, 2.635799256963161491}
 )
 
-// TODO: more metrics, as needed
-// TODO: port GetValue, GetClosestLevel
+// The maximum diagonal is also the maximum diameter of any cell,
+// and also the maximum geometric width (see the comment for widths). For
+// example, the distance from an arbitrary point to the closest cell center
+// at a given level is at most half the maximum diagonal length.
+var (
+	MinDiagMetric = Metric{1, 8 * math.Sqrt2 / 9}
+	AvgDiagMetric = Metric{1, 2.060422738998471683}
+	MaxDiagMetric = Metric{1, 2.438654594434021032}
+
+	// MaxDiagAspect is the maximum diagonal aspect ratio over all cells at any
+	// level, where the diagonal aspect ratio of a cell is defined as the ratio
+	// of its longest diagonal length to its shortest diagonal length.
+	MaxDiagAspect = math.Sqrt(3)
+)
 
 // Value returns the value of the metric at the given level.
 func (m Metric) Value(level int) float64 {
@@ -81,4 +143,16 @@ func (m Metric) MaxLevel(val float64) int {
 		level = 0
 	}
 	return level
+}
+
+// ClosestLevel returns the level at which the metric has approximately the given
+// value. The return value is always a valid level. For example,
+// AvgEdgeMetric.ClosestLevel(0.1) returns the level at which the average cell edge
+// length is approximately 0.1.
+func (m Metric) ClosestLevel(val float64) int {
+	x := math.Sqrt2
+	if m.Dim == 2 {
+		x = 2
+	}
+	return m.MinLevel(x * val)
 }
--- s2/metric_test.go
+++ s2/metric_test.go
@@ -17,13 +17,93 @@ limitations under the License.
 package s2
 
 import (
+	"math"
 	"testing"
 )
 
 func TestMetric(t *testing.T) {
-	// This is not a thorough test.
-	// TODO(dsymonds): Exercise this more.
 	if got := MinWidthMetric.MaxLevel(0.001256); got != 9 {
 		t.Errorf("MinWidthMetric.MaxLevel(0.001256) = %d, want 9", got)
+	}
+
+	// Check that the maximum aspect ratio of an individual cell is consistent
+	// with the global minimums and maximums.
+	if MaxEdgeAspect < 1 {
+		t.Errorf("MaxEdgeAspect = %v, want >= 1", MaxEdgeAspect)
+	}
+	if got := MaxEdgeMetric.Deriv / MinEdgeMetric.Deriv; MaxEdgeAspect > got {
+		t.Errorf("Edge Aspect: %v/%v = %v, want <= %v", MaxEdgeMetric.Deriv, MinEdgeMetric.Deriv, got, MaxDiagAspect)
+	}
+	if MaxDiagAspect < 1 {
+		t.Errorf("MaxDiagAspect = %v, want >= 1", MaxDiagAspect)
+	}
+	if got := MaxDiagMetric.Deriv / MinDiagMetric.Deriv; MaxDiagAspect > got {
+		t.Errorf("Diag Aspect: %v/%v = %v, want <= %v", MaxDiagMetric.Deriv, MinDiagMetric.Deriv, got, MaxDiagAspect)
+	}
+
+	// Check that area is consistent with edge and width.
+	if got := MinWidthMetric.Deriv*MinEdgeMetric.Deriv - 1e-15; MinAreaMetric.Deriv < got {
+		t.Errorf("Min Area: %v*%v = %v, want >= %v", MinWidthMetric.Deriv, MinEdgeMetric.Deriv, got, MinAreaMetric.Deriv)
+	}
+	if got := MaxWidthMetric.Deriv*MaxEdgeMetric.Deriv + 1e-15; MaxAreaMetric.Deriv > got {
+		t.Errorf("Max Area: %v*%v = %v, want <= %v", MaxWidthMetric.Deriv, MaxEdgeMetric.Deriv, got, MaxAreaMetric.Deriv)
+	}
+
+	for level := -2; level <= maxLevel+3; level++ {
+		width := MinWidthMetric.Deriv * math.Pow(2, float64(-level))
+		if level >= maxLevel+3 {
+			width = 0
+		}
+
+		// Check boundary cases (exactly equal to a threshold value).
+		expected := int(math.Max(0, math.Min(maxLevel, float64(level))))
+
+		if MinWidthMetric.MinLevel(width) != expected {
+			t.Errorf("MinWidthMetric.MinLevel(%v) = %v, want %v", width, MinWidthMetric.MinLevel(width), expected)
+		}
+		if MinWidthMetric.MaxLevel(width) != expected {
+			t.Errorf("MinWidthMetric.MaxLevel(%v) = %v, want %v", width, MinWidthMetric.MaxLevel(width), expected)
+		}
+		if MinWidthMetric.ClosestLevel(width) != expected {
+			t.Errorf("MinWidthMetric.ClosestLevel(%v) = %v, want %v", width, MinWidthMetric.ClosestLevel(width), expected)
+		}
+
+		// Also check non-boundary cases.
+		if got := MinWidthMetric.MinLevel(1.2 * width); got != expected {
+			t.Errorf("non-boundary MinWidthMetric.MinLevel(%v) = %v, want %v", 1.2*width, got, expected)
+		}
+		if got := MinWidthMetric.MaxLevel(0.8 * width); got != expected {
+			t.Errorf("non-boundary MinWidthMetric.MaxLevel(%v) = %v, want %v", 0.8*width, got, expected)
+		}
+		if got := MinWidthMetric.ClosestLevel(1.2 * width); got != expected {
+			t.Errorf("non-boundary larger MinWidthMetric.ClosestLevel(%v) = %v, want %v", 1.2*width, got, expected)
+		}
+		if got := MinWidthMetric.ClosestLevel(0.8 * width); got != expected {
+			t.Errorf("non-boundary smaller MinWidthMetric.ClosestLevel(%v) = %v, want %v", 0.8*width, got, expected)
+		}
+	}
+}
+
+func TestMetricSizeRelations(t *testing.T) {
+	// check that min <= avg <= max for each metric.
+	tests := []struct {
+		min Metric
+		avg Metric
+		max Metric
+	}{
+		{MinAngleSpanMetric, AvgAngleSpanMetric, MaxAngleSpanMetric},
+		{MinWidthMetric, AvgWidthMetric, MaxWidthMetric},
+		{MinEdgeMetric, AvgEdgeMetric, MaxEdgeMetric},
+		{MinDiagMetric, AvgDiagMetric, MaxDiagMetric},
+		{MinAreaMetric, AvgAreaMetric, MaxAreaMetric},
+	}
+
+	for _, test := range tests {
+		if test.min.Deriv > test.avg.Deriv {
+			t.Errorf("Min %v > Avg %v", test.min.Deriv, test.avg.Deriv)
+		}
+		if test.avg.Deriv > test.max.Deriv {
+			t.Errorf("Avg %v > Max %v", test.avg.Deriv, test.max.Deriv)
+		}
 	}
 }
