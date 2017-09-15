9ccb2d679eaa00b1aea2f2fe2e06396e1705d093
Robert Snedegar <viking@gmail.com>
2017-09-15 17:47:00 +1000

    s2: Add more internals to shapeutil, and the CrossingType type.
    Signed-off-by: David Symonds <dsymonds@golang.org>

--- s2/shapeutil.go
+++ s2/shapeutil.go
@@ -16,6 +16,134 @@ limitations under the License.
 
 package s2
 
+// CrossingType defines different ways of reporting edge intersections.
+type CrossingType int
+
+const (
+	// CrossingTypeInterior reports intersections that occur at a point
+	// interior to both edges (i.e., not at a vertex).
+	CrossingTypeInterior CrossingType = iota
+
+	// CrossingTypeAll reports all intersections, even those where two edges
+	// intersect only because they share a common vertex.
+	CrossingTypeAll
+
+	// CrossingTypeNonAdjacent reports all intersections except for pairs of
+	// the form (AB, BC) where both edges are from the same ShapeIndex.
+	CrossingTypeNonAdjacent
+)
+
+// referencePointForShape is a helper function for implementing various Shapes
+// ReferencePoint functions.
+//
+// Given a shape consisting of closed polygonal loops, the interior of the
+// shape is defined as the region to the left of all edges (which must be
+// oriented consistently). This function then chooses an arbitrary point and
+// returns true if that point is contained by the shape.
+//
+// Unlike Loop and Polygon, this method allows duplicate vertices and
+// edges, which requires some extra care with definitions. The rule that we
+// apply is that an edge and its reverse edge cancel each other: the result
+// is the same as if that edge pair were not present. Therefore shapes that
+// consist only of degenerate loop(s) are either empty or full; by convention,
+// the shape is considered full if and only if it contains an empty loop (see
+// laxPolygon for details).
+//
+// Determining whether a loop on the sphere contains a point is harder than
+// the corresponding problem in 2D plane geometry. It cannot be implemented
+// just by counting edge crossings because there is no such thing as a point
+// at infinity that is guaranteed to be outside the loop.
+//
+// This function requires that the given Shape have an interior.
+func referencePointForShape(shape Shape) ReferencePoint {
+	if shape.NumEdges() == 0 {
+		// A shape with no edges is defined to be full if and only if it
+		// contains an empty loop.
+		return OriginReferencePoint(shape.NumChains() > 0)
+	}
+	// Define a "matched" edge as one that can be paired with a corresponding
+	// reversed edge. Define a vertex as "balanced" if all of its edges are
+	// matched. In order to determine containment, we must find an unbalanced
+	// vertex. Often every vertex is unbalanced, so we start by trying an
+	// arbitrary vertex.
+	edge := shape.Edge(0)
+
+	if ref, ok := referencePointAtVertex(shape, edge.V0); ok {
+		return ref
+	}
+
+	// That didn't work, so now we do some extra work to find an unbalanced
+	// vertex (if any). Essentially we gather a list of edges and a list of
+	// reversed edges, and then sort them. The first edge that appears in one
+	// list but not the other is guaranteed to be unmatched.
+	n := shape.NumEdges()
+	var edges = make([]Edge, n)
+	var revEdges = make([]Edge, n)
+	for i := 0; i < n; i++ {
+		edge := shape.Edge(i)
+		edges[i] = edge
+		revEdges[i] = Edge{V0: edge.V1, V1: edge.V0}
+	}
+
+	sortEdges(edges)
+	sortEdges(revEdges)
+
+	for i := 0; i < n; i++ {
+		if edges[i].Cmp(revEdges[i]) == -1 { // edges[i] is unmatched
+			if ref, ok := referencePointAtVertex(shape, edges[i].V0); ok {
+				return ref
+			}
+		}
+		if revEdges[i].Cmp(edges[i]) == -1 { // revEdges[i] is unmatched
+			if ref, ok := referencePointAtVertex(shape, revEdges[i].V0); ok {
+				return ref
+			}
+		}
+	}
+
+	// All vertices are balanced, so this polygon is either empty or full. By
+	// convention it is defined to be full if it contains any empty loop.
+	for i := 0; i < shape.NumChains(); i++ {
+		if shape.Chain(i).Length == 0 {
+			return OriginReferencePoint(true)
+		}
+	}
+
+	return OriginReferencePoint(false)
+}
+
+// referencePointAtVertex reports whether the given vertex is unbalanced, and
+// returns a ReferencePoint indicating if the point is contained.
+// Otherwise returns false.
+func referencePointAtVertex(shape Shape, vTest Point) (ReferencePoint, bool) {
+	var ref ReferencePoint
+
+	// Let P be an unbalanced vertex. Vertex P is defined to be inside the
+	// region if the region contains a particular direction vector starting from
+	// P, namely the direction p.Ortho(). This can be calculated using
+	// ContainsVertexQuery.
+
+	containsQuery := NewContainsVertexQuery(vTest)
+	n := shape.NumEdges()
+	for e := 0; e < n; e++ {
+		edge := shape.Edge(e)
+		if edge.V0 == vTest {
+			containsQuery.AddEdge(edge.V1, 1)
+		}
+		if edge.V1 == vTest {
+			containsQuery.AddEdge(edge.V0, -1)
+		}
+	}
+	containsSign := containsQuery.ContainsVertex()
+	if containsSign == 0 {
+		return ref, false // There are no unmatched edges incident to this vertex.
+	}
+	ref.Point = vTest
+	ref.Contained = containsSign > 0
+
+	return ref, true
+}
+
 // containsBruteForce reports whether the given shape contains the given point.
 // Most clients should not use this method, since its running time is linear in
 // the number of shape edges. Instead clients should create a ShapeIndex and use
--- s2/shapeutil_test.go
+++ s2/shapeutil_test.go
@@ -26,11 +26,15 @@ import (
 // parts of testing.
 //
 //  - edgeVectorShape: represents an arbitrary collection of edges.
+//  - laxLoop:         like Loop but allows duplicate vertices & edges,
+//                     more compact representation, faster to initialize.
+//
 //  TODO(roberts): Add remaining testing types here.
 
 // Shape interface enforcement
 var (
 	_ Shape = (*edgeVectorShape)(nil)
+	_ Shape = (*laxLoop)(nil)
 )
 
 // edgeVectorShape is a Shape representing an arbitrary set of edges. It
@@ -65,6 +69,34 @@ func (e *edgeVectorShape) ChainEdge(chainID, offset int) Edge     { return e.edg
 func (e *edgeVectorShape) ChainPosition(edgeID int) ChainPosition { return ChainPosition{edgeID, 0} }
 func (e *edgeVectorShape) dimension() dimension                   { return polylineGeometry }
 
+func TestShapeutilEdgeVectorShapeEdgeAccess(t *testing.T) {
+	shape := &edgeVectorShape{}
+
+	const numEdges = 100
+	for i := 0; i < numEdges; i++ {
+		a := randomPoint()
+		shape.Add(a, randomPoint())
+	}
+	if got, want := shape.NumEdges(), numEdges; got != want {
+		t.Errorf("shape.NumEdges() = %v, want %v", got, want)
+	}
+	if got, want := shape.NumChains(), numEdges; got != want {
+		t.Errorf("shape.NumChains() = %v, want %v", got, want)
+	}
+	if got, want := shape.dimension(), polylineGeometry; got != want {
+		t.Errorf("shape.dimension() = %v, want %v", got, want)
+	}
+
+	for i := 0; i < numEdges; i++ {
+		if got, want := shape.Chain(i).Start, i; got != want {
+			t.Errorf("shape.Chain(i).Start = %v, want %v", got, want)
+		}
+		if got, want := shape.Chain(i).Length, 1; got != want {
+			t.Errorf("shape.Chain(i).Length = %v, want %v", got, want)
+		}
+	}
+}
+
 func TestEdgeVectorShapeSingletonConstructor(t *testing.T) {
 	a := PointFromCoords(1, 0, 0)
 	b := PointFromCoords(0, 1, 0)
@@ -86,7 +118,135 @@ func TestEdgeVectorShapeSingletonConstructor(t *testing.T) {
 	}
 }
 
-// TODO(roberts): Uncomment once LaxLoop and LaxPolygon are added to here.
+// laxLoop represents a closed loop of edges surrounding an interior
+// region. It is similar to Loop except that this class allows
+// duplicate vertices and edges. Loops may have any number of vertices,
+// including 0, 1, or 2. (A one-vertex loop defines a degenerate edge
+// consisting of a single point.)
+//
+// Note that laxLoop is faster to initialize and more compact than
+// Loop, but does not support the same operations as Loop.
+type laxLoop struct {
+	numVertices int
+	vertices    []Point
+}
+
+func laxLoopFromPoints(vertices []Point) *laxLoop {
+	l := &laxLoop{
+		numVertices: len(vertices),
+		vertices:    make([]Point, len(vertices)),
+	}
+	copy(l.vertices, vertices)
+	return l
+}
+
+func laxLoopFromLoop(loop *Loop) *laxLoop {
+	if loop.IsFull() {
+		panic("FullLoops are not yet supported")
+	}
+	if loop.IsEmpty() {
+		return &laxLoop{}
+	}
+
+	l := &laxLoop{
+		numVertices: len(loop.vertices),
+		vertices:    make([]Point, len(loop.vertices)),
+	}
+	copy(l.vertices, loop.vertices)
+	return l
+}
+
+func (l *laxLoop) vertex(i int) Point { return l.vertices[i] }
+func (l *laxLoop) NumEdges() int      { return l.numVertices }
+func (l *laxLoop) Edge(e int) Edge {
+	e1 := e + 1
+	if e1 == l.numVertices {
+		e1 = 0
+	}
+	return Edge{l.vertices[e], l.vertices[e1]}
+
+}
+func (l *laxLoop) dimension() dimension { return polygonGeometry }
+func (l *laxLoop) HasInterior() bool    { return l.dimension() == polygonGeometry }
+func (l *laxLoop) ReferencePoint() ReferencePoint {
+	// ReferencePoint interprets a loop with no vertices as full.
+	if l.numVertices == 0 {
+		return OriginReferencePoint(false)
+	}
+	return referencePointForShape(l)
+}
+func (l *laxLoop) NumChains() int    { return min(1, l.numVertices) }
+func (l *laxLoop) Chain(i int) Chain { return Chain{0, l.numVertices} }
+func (l *laxLoop) ChainEdge(i, j int) Edge {
+	var k int
+	if j+1 == l.numVertices {
+		k = j + 1
+	}
+	return Edge{l.vertices[j], l.vertices[k]}
+}
+func (l *laxLoop) ChainPosition(e int) ChainPosition {
+	return ChainPosition{0, e}
+}
+
+func TestShapeutilLaxLoopEmptyLoop(t *testing.T) {
+	shape := Shape(laxLoopFromLoop(EmptyLoop()))
+
+	if shape.NumEdges() != 0 {
+		t.Errorf("empty laxLoop.NumEdges() = %v, want 0", shape.NumEdges())
+	}
+	if shape.NumChains() != 0 {
+		t.Errorf("empty laxLoop.NumChains() = %v, want 0", shape.NumChains())
+	}
+	if shape.dimension() != polygonGeometry {
+		t.Errorf("laxLoop.dimension() = %v, want %v", shape.dimension(), polygonGeometry)
+	}
+	if shape.ReferencePoint().Contained {
+		t.Errorf("empty laxLoop.ReferencePoint().Contained should be false")
+	}
+}
+
+func TestShapeutilLaxLoopNonEmptyLoop(t *testing.T) {
+	vertices := parsePoints("0:0, 0:1, 1:1, 1:0")
+	shape := Shape(laxLoopFromPoints(vertices))
+	if got, want := len(shape.(*laxLoop).vertices), len(vertices); got != want {
+		t.Errorf("laxLoop.numVertices = %v, want %v", got, want)
+	}
+	if got, want := shape.NumEdges(), len(vertices); got != want {
+		t.Errorf("shape.NumEdges() = %v, want %v", got, want)
+	}
+	if got, want := shape.NumChains(), 1; got != want {
+		t.Errorf("shape.NumChains() = %v, want %v", got, want)
+	}
+	if got, want := shape.Chain(0).Start, 0; got != want {
+		t.Errorf("shape.Chain(0).Start = %v, want %v", got, want)
+	}
+	if got, want := shape.Chain(0).Length, len(vertices); got != want {
+		t.Errorf("shape.Chain(0).Length = %v, want %v", got, want)
+	}
+	for i := 0; i < len(vertices); i++ {
+		if got, want := shape.(*laxLoop).vertex(i), vertices[i]; got != want {
+			t.Errorf("%d. vertex(%d) = %v, want %v", i, i, got, want)
+		}
+		edge := shape.Edge(i)
+		if vertices[i] != edge.V0 {
+			t.Errorf("%d. edge.V0 = %v, want %v", i, edge.V0, vertices[i])
+		}
+		if got, want := edge.V1, vertices[(i+1)%len(vertices)]; got != want {
+			t.Errorf("%d. edge.V1 = %v, want %v", i, got, want)
+		}
+	}
+	if got, want := shape.dimension(), polygonGeometry; got != want {
+		t.Errorf("shape.dimension() = %v, want %v", got, want)
+	}
+	if !shape.HasInterior() {
+		t.Errorf("shape.HasInterior() = false, want true")
+	}
+	if shape.ReferencePoint().Contained {
+		t.Errorf("shape.ReferencePoint().Contained = true, want false")
+	}
+}
+
+// TODO(roberts): Uncomment once LaxPolyline and LaxPolygon are added to here.
 /*
 func TestShapeutilContainsBruteForceNoInterior(t *testing.T) {
 	// Defines a polyline that almost entirely encloses the point 0:0.
