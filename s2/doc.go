4a14792eb7e8a5c2c680bc0b8134d3ce9eac847e
Robert Snedegar <viking@gmail.com>
2019-09-26 06:55:00 +1000

    s2: Update doc to reference the s2geometry.io site instead of the old google doc.
    Signed-off-by: David Symonds <dsymonds@golang.org>

--- s2/doc.go
+++ s2/doc.go
@@ -13,17 +13,17 @@
 // limitations under the License.
 
 /*
-Package s2 implements types and functions for working with geometry in S² (spherical geometry).
+Package s2 is a library for working with geometry in S² (spherical geometry).
 
-Its related packages, parallel to this one, are s1 (operates on S¹), r1 (operates on ℝ¹)
-and r3 (operates on ℝ³).
+Its related packages, parallel to this one, are s1 (operates on S¹), r1 (operates on ℝ¹),
+r2 (operates on ℝ²) and r3 (operates on ℝ³).
 
 This package provides types and functions for the S2 cell hierarchy and coordinate systems.
 The S2 cell hierarchy is a hierarchical decomposition of the surface of a unit sphere (S²)
 into ``cells''; it is highly efficient, scales from continental size to under 1 cm²
 and preserves spatial locality (nearby cells have close IDs).
 
-A presentation that gives an overview of S2 is
-https://docs.google.com/presentation/d/1Hl4KapfAENAOf4gv-pSngKwvS_jwNVHRPZTTDzXXn6Q/view.
+More information including an in-depth introduction to S2 can be found on the
+S2 website https://s2geometry.io/
 */
 package s2
