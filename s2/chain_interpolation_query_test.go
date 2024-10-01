86ce61b945fbb66b67d985fca0c60898d6904a2a
Pavlov Vladimir <pavlovvova923@gmail.com>
2024-10-01 12:53:59 +0300

    adding tests

--- s2/chain_interpolation_query_test.go
+++ s2/chain_interpolation_query_test.go
@@ -31,4 +31,112 @@ func testSimplePolylines(t *testing.T) {
 	abcQuery := InitS2ChainInterpolationQuery(abcPolyline, 0)
 	bbQuery := InitS2ChainInterpolationQuery(bbPolyline, 0)
 	ccQuery := InitS2ChainInterpolationQuery(ccPolyline, 0)
+
+	type reslut struct {
+		point    Point
+		edgeID   int
+		distance s1.Angle
+	}
+	distances := []float64{
+		-1.,
+		0.,
+		1.0e-8,
+		latitudeB / 2,
+		latitudeB - 1.0e-7,
+		latitudeB,
+		latitudeB + 1.0e-5,
+		latitudeB + 0.5,
+		latitudeC - 10.e-7,
+		latitudeC,
+		latitudeC + 10.e-16,
+		1.e6,
+	}
+
+	groundTruth := make([]reslut, len(distances))
+	for i, distance := range distances {
+		lat := max(.0, min(totalLengthAbc, distance))
+		point := PointFromLatLng(LatLngFromDegrees(lat, 0))
+		var edgeID int
+		if distance < latitudeB {
+			edgeID = 0
+		} else {
+			edgeID = 1
+		}
+		groundTruth[i] = reslut{point: point, edgeID: edgeID, distance: s1.Angle(distance)}
+	}
+
+	lengthEmpty, err := emptyQuery.GetLength()
+	if err != nil {
+		t.Fatal(err)
+	}
+	lengthAc, err := acQuery.GetLength()
+	if err != nil {
+		t.Fatal(err)
+	}
+	lengthAbc, err := abcQuery.GetLength()
+	if err != nil {
+		t.Fatal(err)
+	}
+	lengthBb, err := bbQuery.GetLength()
+	if err != nil {
+		t.Fatal(err)
+	}
+	lengthCc, err := ccQuery.GetLength()
+	if err != nil {
+		t.Fatal(err)
+	}
+	degreesEmpty := lengthEmpty.Degrees()
+	degreesAc := lengthAc.Degrees()
+	degreesAbc := lengthAbc.Degrees()
+	degreesBb := lengthBb.Degrees()
+	degreesCc := lengthCc.Degrees()
+
+	point, edgeID, distance, err := uninitializedQuery.AtFraction(0)
+	if err == nil {
+		t.Fatalf("got %v, want error", point)
+	}
+	point, edgeID, distance, err = acQuery.AtDistance(s1.InfAngle())
+	if err == nil {
+		t.Fatalf("got %v, want error", point)
+	}
+
+	distanceResult := make([]reslut, len(distances))
+
+	for i, distance := range distances {
+		totalFraction := distance / totalLengthAbc
+
+		distancePoint, distanceEdgeID, newDistance, err := emptyQuery.AtFraction(totalFraction)
+		if err != nil && i != len(distances)-1 {
+			t.Fatal(err)
+		}
+		distanceResult[i] = reslut{point: distancePoint, edgeID: distanceEdgeID, distance: newDistance}
+	}
+
+	if degreesEmpty >= kEpsilon {
+		t.Errorf("got %v, want %v", degreesEmpty, kEpsilon)
+	}
+
+	if !float64Near(float64(lengthAc), totalLengthAbc, kEpsilon) {
+		t.Errorf("got %v, want %v", lengthAc, totalLengthAbc)
+	}
+
+	if !float64Near(float64(lengthAbc), totalLengthAbc, kEpsilon) {
+		t.Errorf("got %v, want %v", lengthAbc, totalLengthAbc)
+	}
+
+	if lengthBb >= kEpsilon {
+		t.Errorf("got %v, want %v", lengthBb, kEpsilon)
+	}
+
+	if lengthCc >= kEpsilon {
+		t.Errorf("got %v, want %v", lengthBb, kEpsilon)
+	}
+
+	if point.Angle(c.Vector) >= kEpsilon {
+		t.Errorf("got %v, want %v", point, kEpsilon)
+	}
+
+	for i := 0; i < len(groundTruth); i++ {
+
+	}
 }
