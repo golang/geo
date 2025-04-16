809fb2901a700d3ba79c19cee08daa941065f30b
Robert Snedegar <rsned@users.noreply.github.com>
2025-04-16 15:02:53 -0700

    Update stuv_test.go  to explicitly use all three switch cases and remove nolint tag.

--- s2/stuv_test.go
+++ s2/stuv_test.go
@@ -115,7 +115,7 @@ func TestFaceUVToXYZ(t *testing.T) {
 		if !center.ApproxEqual(unitNorm(face).Vector) {
 			t.Errorf("faceUVToXYZ(%d, 0, 0) != unitNorm(%d), should be equal", face, face)
 		}
-		switch center.LargestComponent() { // nolint exhaustive - default catches the 'missing' case.
+		switch center.LargestComponent() {
 		case r3.XAxis:
 			if math.Abs(center.X) != 1 {
 				t.Errorf("%v.X = %v, want %v", center, math.Abs(center.X), 1)
@@ -124,7 +124,7 @@ func TestFaceUVToXYZ(t *testing.T) {
 			if math.Abs(center.Y) != 1 {
 				t.Errorf("%v.Y = %v, want %v", center, math.Abs(center.Y), 1)
 			}
-		default:
+		case r3.ZAxis:
 			if math.Abs(center.Z) != 1 {
 				t.Errorf("%v.Z = %v, want %v", center, math.Abs(center.Z), 1)
 			}
